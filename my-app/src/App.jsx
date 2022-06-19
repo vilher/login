0ddb99d71cea2543cec434ad0aeb5eb829261188
vilher <viktolim@gmail.com>
2022-06-19 07:46:53 +0300

    login user interface

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./Form";
+
+var userIsRegistered = true;
+function App() {
+  return (
+    <div className="container">
+<Form isRegistered={userIsRegistered}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Form.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Input from "./Input";
+
+function Form(props) {
+  return (
+    <form className="form">
+        <Input type="text" placeholder="Username"/>
+        <Input type="password" placeholder="Password"/>
+        {!props.isRegistered && <Input type="password" placeholder="Confirm Password"/>}
+      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
+    </form>
+  );
+}
+
+export default Form;
